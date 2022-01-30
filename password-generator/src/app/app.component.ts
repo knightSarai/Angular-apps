f31ad1e60a31176f141f84a1158e2c4ab18d7a2f
knight <knight.sarai.dev@gmail.com>
2022-01-30 23:48:16 +0200

    Generate random password based on user selection

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +8,49 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'password-generator';
   password = ''
+  passwordLength = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
 
-  generatePassword(){
-    this.password = 'knight'
+  onChangeLength(event: Event) {
+    const inputValue = (<HTMLInputElement>event.target).value;
+    const parsedValue = parseInt(inputValue);
+    if (!isNaN(parsedValue)) this.passwordLength = parsedValue;
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  generatePassword() {
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    this.password = this.passwordLength > 0 ?
+      Array(this.passwordLength)
+        .fill(0)
+        .map(() => {
+          const random = Math.floor(Math.random() * 3);
+          switch (random) {
+            case 0:
+              return this.useLetters ? letters[Math.floor(Math.random() * letters.length)] : '';
+            case 1:
+              return this.useNumbers ? numbers[Math.floor(Math.random() * numbers.length)] : '';
+            case 2:
+              return this.useSymbols ? symbols[Math.floor(Math.random() * symbols.length)] : '';
+            default:
+              return '';
+          }
+        })
+        .join('') : '';
   }
 }
